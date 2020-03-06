e03033cb2b49a362cd7876bbf74b24ceb1a25839
DmitIW <iwahnencko1996@icloud.com>
2020-03-06 13:29:02 +0300

    Add PocoHandler for message queue web interface

--- src/SimplePocoHandler.cpp
+++ src/SimplePocoHandler.cpp
@@ -1,5 +1,21 @@
 //
 // Created by dmitri on 05.03.2020.
 //
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <cstring>
+#include <cassert>
+#include <iostream>
+#include <Poco/Net/StreamSocket.h>
+
 #include "SimplePocoHandler.h"
+
+
+namespace utility {
+    class Buffer {
+    private:
+
+    };
+}
 
--- src/SimplePocoHandler.h
+++ src/SimplePocoHandler.h
@@ -8,6 +8,35 @@
 #include <include/amqpcpp.h>
 #include <memory>
 
-struct SimplePocoHandlerImpl;
+class SimplePocoHandlerImpl;
+class SimplePocoHandler: public AMQP::ConnectionHandler {
+private:
+    std::shared_ptr<SimplePocoHandlerImpl> m_impl;
+public:
+    static constexpr size_t MB = 1024 * 1024;
+    static constexpr size_t BUFFER_SIZE = 8 * MB;
+    static constexpr size_t TEMP_BUFFER_SIZE = MB;
+
+    SimplePocoHandler(const std::string& host, uint16_t port);
+    ~SimplePocoHandler();
+
+    void loop();
+    void quit();
+
+    bool connected() const;
+
+    SimplePocoHandler(const SimplePocoHandler&) = delete;
+    SimplePocoHandler& operator=(const SimplePocoHandler&) = delete;
+
+private:
+
+
+    void onData(AMQP::Connection* connection, const char* data, size_t size) override;
+    void onReady(AMQP::Connection* connection) override;
+    void onError(AMQP::Connection* connection, const char* message) override;
+    void onClosed(AMQP::Connection* connection) override;
+
+    void close();
+};
 
 #endif //HUFFMANCODE_SIMPLEPOCOHANDLER_H
